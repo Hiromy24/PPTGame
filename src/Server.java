1f64d9779698d9f8eba4cca0f95e0436f7b220c8
Hiromy24 <henryillescas24@gmail.com>
2024-11-24 23:42:45 +0100

    working gamepsw

--- src/Server.java
+++ src/Server.java
@@ -10,7 +10,7 @@ public class Server {
     public static final int PUERTO = 2024;
     private static final List<Game> games = new ArrayList<>();
     private static int gameNumber = 0;
-    private static Map<Socket, Integer> waitingPlayers = new HashMap<>();
+    private static Map<Integer, Queue<Socket>> waitingPlayers = new HashMap<>();
     public static void main(String[] args) {
         System.out.println("            SERVER APP            ");
         System.out.println("----------------------------------");
@@ -19,53 +19,44 @@ public class Server {
         try (ServerSocket serverSocket = new ServerSocket()) {
             serverSocket.bind(sa);
             while (true) {
+
                 Socket player = serverSocket.accept();
                 InputStreamReader input = new InputStreamReader(player.getInputStream());
                 BufferedReader bf = new BufferedReader(input);
-                if (bf.readLine().equals("true")){
-                    Game game =new Game();
-                    String psw = bf.readLine();
-                    gameFinder(game, player, Integer.parseInt(psw));
-                }else{
-                    waitingPlayers.put(player, Integer.valueOf(bf.readLine()));
-                }
+                new Thread(() -> {
+                    int psw = 0;
+                    try {
+                        psw = Integer.parseInt(bf.readLine());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    synchronized (waitingPlayers) {
+                        waitingPlayers.putIfAbsent(psw, new LinkedList<>());
+                        waitingPlayers.get(psw).add(player);
+                    }
+                    if (waitingPlayers.get(psw).size() >= 2){
+                        Socket player1 = waitingPlayers.get(psw).poll();
+                        Socket player2 = waitingPlayers.get(psw).poll();
+                        Game game = new Game();
+                        try {
+                            game.addSocket(player1);
+                            game.addSocket(player2);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                        synchronized (games) {
+                            games.add(game);
+                            game.start();
+                            gameNumber++;
+                        }
+                    }
+                }).start();
+
             }
         } catch (IOException e) {
             System.err.println("SERVER: Error starting server: " + e.getMessage());
         }
     }
-    private static void gameFinder( Game game,Socket gameOwner, int psw){
-        new Thread(() -> {
-            try {
-                int gamePsw = 0;
-                if (psw != 1 ){
-                    gamePsw = psw;
-                    game.setGamePsw(psw);
-                    gamePsw = game.getGamePsw();
-                    game.addSocket(gameOwner);
-                }
 
-                while (game.playersConected() !=2) {
-                    for (Map.Entry<Socket, Integer> entry : waitingPlayers.entrySet()) {
-                        Socket player = entry.getKey();
-                        InputStreamReader inputActual = new InputStreamReader(player.getInputStream());
-                        BufferedReader bfActual = new BufferedReader(inputActual);
-                        if (gamePsw == entry.getValue()) {
-                            game.addSocket(player);
-                            waitingPlayers.remove(player);
-                        }
-                    }
-                }
-
-                synchronized (games) {
-                    games.add(game);
-                    game.start();
-                    gameNumber++;
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-       }).start();
-    }
 
 }
